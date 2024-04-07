3b486bc002b5a3e7fc90ea4c3dac8f9830930622
CsengeOnCode <161061835+CsengeOnCode@users.noreply.github.com>
2024-04-07 23:11:41 +0100

    Added a link to main page

--- javascript/script.js
+++ javascript/script.js
@@ -55,8 +55,10 @@ function updateCity(event) {
           </div>
           <div class="time">${cityTime.format(
             "h:mm:ss"
-          )}<small>${cityTime.format("A")}</small></div>
-        </div>`;
+          )}<small>${cityTime.format(" A")}</small></div>
+        </div>
+        <a href="index.html"> 🔙 All Cities</a>
+        `;
 }
 
 updatetime();
